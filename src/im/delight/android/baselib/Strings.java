f9fb06cd55317bac31b6cef3c8478b91aaafe937
Marco <marco1@live.de>
2014-03-14 15:40:42 +0100

    Added ROT-13 for strings

--- src/im/delight/android/baselib/Strings.java
+++ src/im/delight/android/baselib/Strings.java
@@ -62,5 +62,26 @@ public class Strings {
 	public static String padLeft(String str, int length) {
 		return String.format("%1$"+length+"s", str);
 	}
+	
+	public static String rot13(String input) {
+		StringBuilder out = new StringBuilder();
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (c >= 'a' && c <= 'm') {
+				c += 13;
+			}
+			else if (c >= 'A' && c <= 'M') {
+				c += 13;
+			}
+			else if (c >= 'n' && c <= 'z') {
+				c -= 13;
+			}
+			else if (c >= 'N' && c <= 'Z') {
+				c -= 13;
+			}
+			out.append(c);
+		}
+		return out.toString();
+	}
 
 }
